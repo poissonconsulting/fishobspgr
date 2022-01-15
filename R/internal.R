48eeaba63eb0fcd161b6b551b81df9e350b7b134
Seb Dalgarno <seb.dalgarno@gmail.com>
2022-01-14 21:12:50 -0800

    reset

--- R/internal.R
+++
@@ -1,7 +0,0 @@
-api_url <- function() "https://features.hillcrestgeo.ca/"
-
-gh_user <- function() "http://github.com/poissonconsulting/fwapgr"
-
-path_obs <- function() "bcfishobs"
-
-path_pass <- function() "bcfishpass"
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(fishobspgr)
-
-test_check("fishobspgr")
