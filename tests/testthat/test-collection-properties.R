885fa92876cb94f47cf2e8cc4da2ca00332f8d52
Seb Dalgarno <seb.dalgarno@gmail.com>
2022-01-15 19:14:34 -0800

    remove snapshot causing issue in ubuntu

--- tests/testthat/test-collection-properties.R
+++ tests/testthat/test-collection-properties.R
@@ -11,8 +11,3 @@ test_that("passage collection properties works", {
   properties <- fop_collection_passage_properties(collection_id)
   expect_s3_class(properties, "tbl_df")
 })
-
-test_that("collection_properties haven't changed for observation events", {
-  properties <- fop_collection_observation_properties("bcfishobs.fiss_fish_obsrvtn_events_sp")
-  expect_snapshot_data(properties, "properties")
-})
